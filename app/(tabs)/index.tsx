0ee8d3ca61b4e4eab0bf14eef98b5460b6828257
llortegall <ivanortega_97@hotmail.com>
2025-09-10 17:49:02 -0500

    refactor HomeScreen component to use HabitCard and HabitGreeting, removing unnecessary text elements

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import HabitCard from '@/components/HabitCard';
+import HabitGreeting from '@/components/HabitGreeting';
+import { StyleSheet, View } from 'react-native';
 
 export default function HomeScreen() {
   const nombre = "React Native";
@@ -7,21 +8,27 @@ export default function HomeScreen() {
   const isPremium = true;
   const messages = 5;
 
+  const habits = [
+    { id: 1, title: "Drink Water", streak: 10, isCompleted: true },
+    { id: 2, title: "Exercise", streak: 5, isCompleted: false },
+    { id: 3, title: "Read Books", streak: 15, isCompleted: true },
+    { id: 4, title: "Meditate", streak: 3, isCompleted: false },
+    { id: 5, title: "Sleep Early", streak: 7, isCompleted: true },
+  ]
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Mi texto: {nombre}
-      </Text>
-      <Text style={styles.subtitle}>
-        Edad: {edad}
-      </Text>
-      <Text style={styles.subtitle}>
-        {isPremium ? "Usuario Premium" : "Usuario Free"}
-      </Text>
-      <Text style={styles.subtitle}>
-        segundo texto de ejemplo
-      </Text>
-      <Link href={"/(tabs)/about"} style={styles.buttonAbout}>ver sobre mí</Link>
+      <HabitGreeting habitName="Ivan Ortega" />
+      <View style={{ gap: 12, marginTop: 16, width: '100%' }}>
+        {habits.map(habit => (
+          <HabitCard
+            key={habit.id}
+            title={habit.title}
+            streak={habit.streak}
+            isCompleted={habit.isCompleted}
+          />
+        ))}
+      </View>
     </View>
   );
 }
